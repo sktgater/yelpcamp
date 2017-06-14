b069d1c3c2382c658d2019c5609915bb9c0375ab
Li Wang <sktgater@gmail.com>
2017-06-14 01:47:39 +0000

    Add user model, login/logout routes

--- app.js
+++ app.js
@@ -8,17 +8,39 @@ var express = require("express"),
     methodOverride = require("method-override"),
     expressSanitizer = require("express-sanitizer"),
     seedDB = require("./seeds"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     // Load Model Modules
     Campground = require("./models/campground"),
-    Comment = require("./models/comment");
-    //User = require("./models/user");
+    Comment = require("./models/comment"),
+    User = require("./models/user");
 
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
+app.use(express.static(__dirname + "/public"));     // specific current directory/public
 seedDB();
+
+// Passport Config
+app.use(require("express-session")({
+    secret: "Once again rusty wins",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Middleware that sets currentUser on each route
+// Called on every route
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp server has started!");
@@ -36,7 +58,11 @@ app.get("/campgrounds", function(req, res){
             console.log(err);
         }
         else{
-            res.render("campgrounds/index", {campgrounds: campgrounds});
+            res.render("campgrounds/index", 
+            {
+                campgrounds: campgrounds,
+                currentUser: req.user
+            });
         }
     });
 });
@@ -81,7 +107,7 @@ app.get("/campgrounds/:id", function(req, res){
 // ===========================
 // COMMENTS routes
 // ===========================
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     //   find campground by ID
     Campground.findById(req.params.id, function(err, campground){
         if (err){console.log(err);}
@@ -91,7 +117,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     // lookup campground using ID
     Campground.findById(req.params.id, function(err, campground){
        if (err){
@@ -113,3 +139,55 @@ app.post("/campgrounds/:id/comments", function(req, res){
         }
     });
 })
+//===================
+// AUTH routes
+//===================
+// show register form
+app.get("/register", function(req, res){
+    res.render("register");
+})
+
+// handle signup functionalitity
+app.post("/register", function(req, res){
+    // has user object with username filled only
+    var newUser = new User({username: req.body.username});
+    // register() handles password logic
+    User.register(newUser, req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// handle login functionalitity
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds", 
+        failureRedirect: "/login"
+    }), function(req, res){}
+);
+
+// logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+// Middleware function to test if correct user logged in
+function isLoggedIn(req, res, next){
+    // if logged in, call next function
+    if (req.isAuthenticated()){
+        return next();
+    }
+    // not logged in, redirect login page
+    res.redirect("/login");
+}
